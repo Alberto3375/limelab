a8f195c19677781ab5832e6a2c0773a3d479f15d
Alberto3375 <127156968+Alberto3375@users.noreply.github.com>
2025-08-19 13:58:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`✅ Servidor corriendo en el puerto ${port}`);
 });
